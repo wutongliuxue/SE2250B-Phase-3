1d436a831ee39891bcd437eb7927644ee82ad162
wutongliuxue <wutongliuxue@163.com>
2022-03-29 11:58:07 -0400

    Switch Scene
    From outside map to teleport to home base
    
    -- Tong Wu

---
+++ Project Version 4.0.2 4/Assets/_Scripts/__Inventory/Supply.cs
@@ -0,0 +1,24 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class Supply : MonoBehaviour, IInventoryItem
+// {
+//     public string Name{
+//         get{
+//             return "Supply";
+//         }
+//     }
+
+//     public Sprite _Image = null;
+
+//     public Sprite Image{
+//         get{
+//             return _Image;
+//         }
+//     }
+
+//     public void OnPickUp(){
+//         gameObject.SetActive(false);
+//     }
+// }
