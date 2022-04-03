a9385169df54a4e23423cc577650cf396ccf1e57
adarwis7 <90807429+adarwis7@users.noreply.github.com>
2022-04-03 04:54:06 -0400

    Bunch of edits

--- Phase 3 4.0.2 4 2/Assets/_Scripts/_GeneralMonsterAndCatC#/AI.cs
+++ Phase 3 4.0.2 4 2/Assets/_Scripts/_GeneralMonsterAndCatC#/AI.cs
@@ -3,26 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class AIZombie : MonoBehaviour
+public class AI : MonoBehaviour
 {
     public NavMeshAgent enemy;
     public Transform Player;
-    public GameObject RightFist;
-    public GameObject LeftFist;
+    
     // Start is called before the first frame update
     void Start()
     {
         enemy = GetComponent<NavMeshAgent>();
     }
-    public void activatePunsh(){
-        RightFist.GetComponent<Collider>().enabled = true;
-        LeftFist.GetComponent<Collider>().enabled = true;
 
-    }
-    public void deactivatePunsh(){
-        RightFist.GetComponent<Collider>().enabled = false;
-        LeftFist.GetComponent<Collider>().enabled = false;
-    }
 
     // Update is called once per frame
     void Update()
