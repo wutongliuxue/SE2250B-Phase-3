d5d0d1028328bca03503f5d0c27c6060c526d026
wutongliuxue <wutongliuxue@163.com>
2022-04-05 15:03:33 -0400

    N/A
    -- Tong Wu

--- Phase 3 Howard Version/Assets/_Scripts/__AIZombieC#/Death.cs
+++ Phase 3 Howard Version/Assets/_Scripts/__AIZombieC#/Death.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Death : MonoBehaviour
 {
+   private int k=0;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +15,7 @@ public class Death : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
       void OnCollisionEnter(Collision collision){
         
